862fa9ce350874161555c869231069e96af92bed
Nicholas Carrigan (he/him) <nhcarrigan@gmail.com>
2021-12-01 08:26:20 -0800

    feat: solution for day 1 (#3)

---
+++ src/modules/2021/day01.ts
@@ -0,0 +1,37 @@
+import { SolutionFunction } from "../../interfaces/SolutionFunction";
+import { getPuzzleInput } from "../../utils/getPuzzleInput";
+
+export const sonarSweep: SolutionFunction = async () => {
+  const answer = { partOne: "unsolved", partTwo: "unsolved" };
+  const input = await getPuzzleInput("2021", "01");
+  // This is a list of numbers so we need to convert them.
+  const parsed = input.split("\n").map((el) => parseInt(el, 10));
+
+  // Part 1
+
+  let increases = 0;
+
+  for (let i = 1; i < parsed.length; i++) {
+    if (parsed[i] > parsed[i - 1]) {
+      increases++;
+    }
+  }
+
+  answer.partOne = increases.toString();
+
+  // Part 2
+
+  let sumsAreGreater = 0;
+
+  for (let i = 0; i < parsed.length - 3; i++) {
+    const leftSum = parsed[i] + parsed[i + 1] + parsed[i + 2];
+    const rightSum = parsed[i + 1] + parsed[i + 2] + parsed[i + 3];
+    if (rightSum > leftSum) {
+      sumsAreGreater++;
+    }
+  }
+
+  answer.partTwo = sumsAreGreater.toString();
+
+  return answer;
+};
--- src/utils/importSolutions.ts
+++ src/utils/importSolutions.ts
@@ -1,5 +1,5 @@
 import { SolutionFunction } from "../interfaces/SolutionFunction";
-import { sonarSweep } from "../modules/2021/day1";
+import { sonarSweep } from "../modules/2021/day01";
 
 export const solutionData: { [key: string]: SolutionFunction[] } = {
   "2021": [sonarSweep],

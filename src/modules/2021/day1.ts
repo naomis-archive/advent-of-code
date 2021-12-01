2ae33afba25ba72cbf8829c0a98ac8e55d8ec058
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-12-01 07:42:58 -0800

    temp: ignore unused var for now

--- src/modules/2021/day1.ts
+++ src/modules/2021/day1.ts
@@ -5,6 +5,7 @@ import { SolutionFunction } from "../../interfaces/SolutionFunction";
 export const sonarSweep: SolutionFunction = async () => {
   const answer = { partOne: "unsolved", partTwo: "unsolved" };
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const input = await fetch("url here");
 
   return answer;

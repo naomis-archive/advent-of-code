68b61cd9d4c4cdb5ea4e07280895fb2378eb7ea4
Nicholas Carrigan (he/him) <nhcarrigan@gmail.com>
2021-12-02 07:10:43 -0800

    feat: 2021 Day 02 (#10)

--- src/utils/importSolutions.ts
+++ src/utils/importSolutions.ts
@@ -1,6 +1,7 @@
 import { SolutionFunction } from "../interfaces/SolutionFunction";
 import { sonarSweep } from "../modules/2021/day01";
+import { dive } from "../modules/2021/day02";
 
 export const solutionData: { [key: string]: SolutionFunction[] } = {
-  "2021": [sonarSweep],
+  "2021": [sonarSweep, dive],
 };

62bd341db67c3a0e8fede5f5197eec3ca7c2c482
Nicholas Carrigan (he/him) <nhcarrigan@gmail.com>
2021-12-03 09:19:00 -0800

    feat: 2021 day 03 (#13)

--- src/utils/importSolutions.ts
+++ src/utils/importSolutions.ts
@@ -2,8 +2,9 @@ import { SolutionFunction } from "../interfaces/SolutionFunction";
 import { reportRepair } from "../modules/2020/day01";
 import { sonarSweep } from "../modules/2021/day01";
 import { dive } from "../modules/2021/day02";
+import { binaryDiagonstic } from "../modules/2021/day03";
 
 export const solutionData: { [key: string]: SolutionFunction[] } = {
   "2020": [reportRepair],
-  "2021": [sonarSweep, dive],
+  "2021": [sonarSweep, dive, binaryDiagonstic],
 };
--- test/2021.spec.ts
+++ test/2021.spec.ts
@@ -2,6 +2,7 @@ import { assert } from "chai";
 
 import { sonarSweep } from "../src/modules/2021/day01";
 import { dive } from "../src/modules/2021/day02";
+import { binaryDiagonstic } from "../src/modules/2021/day03";
 
 suite("2021 Solutions", () => {
   test("Day 01", async () => {
@@ -30,5 +31,23 @@ forward 2`;
     const expected = { partOne: "150", partTwo: "900" };
     const actual = await dive(data);
     assert.deepEqual(actual, expected, "Day 02 does not pass the mock data.");
+  });
+
+  test("Day 03", async () => {
+    const data = `00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010`;
+    const expected = { partOne: "198", partTwo: "230" };
+    const actual = await binaryDiagonstic(data);
+    assert.deepEqual(actual, expected, "Day 03 does not pass the mock data.");
   });
 });

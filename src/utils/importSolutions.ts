88b21336752309040d72a326c0c80eaa4ab987a7
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-12-05 05:54:26 -0800

    feat: 2021 Day 05

--- src/utils/importSolutions.ts
+++ src/utils/importSolutions.ts
@@ -4,8 +4,9 @@ import { sonarSweep } from "../modules/2021/day01";
 import { dive } from "../modules/2021/day02";
 import { binaryDiagonstic } from "../modules/2021/day03";
 import { giantSquid } from "../modules/2021/day04";
+import { hydrothermalVenture } from "../modules/2021/day05";
 
 export const solutionData: { [key: string]: SolutionFunction[] } = {
   "2020": [reportRepair],
-  "2021": [sonarSweep, dive, binaryDiagonstic, giantSquid],
+  "2021": [sonarSweep, dive, binaryDiagonstic, giantSquid, hydrothermalVenture],
 };
